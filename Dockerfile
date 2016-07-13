2caf93297addc966db54ec1fa2cf78176a013c3c
randomcoww <randomcoww@ipcdebian.dyn.local>
2016-07-13 01:02:42 -0700

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,22 @@ FROM debian:sid
 
 ENV DEBIAN_FRONTEND noninteractive
 
+ENV MUSIC_DIR /mpd/music
+ENV PLAYLISTS_DIR /mpd/playlists
+ENV CACHE_DIR /mpd/cache
+
 RUN \
     apt-get update -qq && \
     apt-get install -qqy --no-install-recommends mpd && \
+    rm -rf /var/lib/mpd/* && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* && \
-    rm -rf /var/lib/mpd/*
+    mkdir -p $MUSIC_DIR $PLAYLISTS_DIR $CACHE_DIR
 
 COPY mpd.conf /etc/mpd.conf
 
 EXPOSE 8000 6600
+VOLUME $MUSIC_DIR $PLAYLISTS_DIR $CACHE_DIR
 
 COPY entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,15 @@
 #!/usr/bin/env bash
 
-set -o nounset
-
-while getopts ":m:p:c:" opt; do
-  case "$opt" in
-    m) export music=${OPTARG} ;;
-    p) export playlists=${OPTARG} ;;
-    c) export cache=${OPTARG} ;;
-  esac
-done
-
 
 ## expect music passed in from mount, fail otherwise (mount not available?)
 
-[[ "$(ls -A $music)" ]] ||  exit
+[[ "$(ls -A $MUSIC_DIR)" ]] ||  exit
 
-ln -sf $music /var/lib/mpd/music
-ln -sf $playlists /var/lib/mpd/playlists
-ln -sf $cache /var/lib/mpd/cache
-
-touch $cache/{db_file,state_file,tag_cache} \
-  && chmod 0777 -R $cache \
-  && chown -R mpd $cache
+touch $CACHE_DIR/{tag_cache,state,sticker.sql} \
+  && chmod -R 0777 $CACHE_DIR \
+  && chown -R mpd $CACHE_DIR
 
 
 ## start
 
-exec mpd --no-daemon --stdout --verbose /etc/mpd.conf
+exec mpd --no-daemon --stdout --verbose /etc/mpd.conf "$@"
