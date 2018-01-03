72629dd922e95488700ce967057a70cd2aff277f
randomcoww <etherblimp@gmail.com>
2018-01-03 02:14:23 -0800

    fix touch files under cache/

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 ## expect music passed in from mount, fail otherwise (mount not available?)
 [[ "$(ls -A /mpd/music)" ]] ||  exit
 
-touch /mpd/cache/{tag_cache,state,sticker.sql} \
-  && chmod -R 0777 /mpd/cache \
-  && chown -R mpd /mpd/cache
+touch \
+  /mpd/cache/tag_cache \
+  /mpd/cache/state \
+  /mpd/cache/sticker.sql
+
+chown -R mpd /mpd/cache
 
 ## start
-exec mpd --no-daemon --stdout --verbose /etc/mpd.conf "$@"
+exec mpd --no-daemon --stdout /etc/mpd.conf "$@"
